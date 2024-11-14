6a4c8021ea7f8308c4893fff2347d37344d5ce82
Sabyasachi Bhoi <sabyabhoi@uber.com>
2024-11-14 22:52:28 +0530

    Implement dfs

--- src/graph.rs
+++ src/graph.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::{self, format, Display},
+};
 
 pub struct Graph {
     root: String,
@@ -30,16 +33,62 @@ impl Graph {
 
         Graph { root, adj }
     }
+
+    pub fn dfs(&self) -> Vec<String> {
+        let mut visited = HashSet::new();
+        let mut result = Vec::new();
+        let mut s = String::new();
+        self.dfs_recursive(&self.root, &mut visited, &mut result, &mut s);
+        result
+    }
+
+    fn dfs_recursive(
+        &self,
+        node: &String,
+        visited: &mut HashSet<String>,
+        result: &mut Vec<String>,
+        formatting_string: &mut String,
+    ) {
+        // Mark the node as visited
+        visited.insert(node.clone());
+        println!("{}{}", formatting_string, node);
+        result.push(node.clone()); // Add node to result list
+
+        let mut check: bool = false;
+        if formatting_string.ends_with("├─") {
+            // change the last character of formatting_str to a whitespace
+            formatting_string.pop();
+            formatting_string.pop();
+            formatting_string.push_str("│ ");
+            check = true;
+        }
+        formatting_string.push_str("├─");
+
+        // Recursively visit all adjacent nodes
+        if let Some(neighbors) = self.adj.get(node) {
+            for neighbor in neighbors {
+                if !visited.contains(neighbor) {
+                    self.dfs_recursive(neighbor, visited, result, formatting_string);
+                }
+            }
+        }
+
+        formatting_string.pop();
+        formatting_string.pop();
+        if check {
+            formatting_string.pop();
+            formatting_string.pop();
+            formatting_string.push_str("├─");
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn get_test_edges() {
-        // Arrange
-        let edges: Vec<(String, Option<String>)> = vec![
+    fn get_test_edges() -> Vec<(String, Option<String>)> {
+        vec![
             ("master".to_string(), None),
             ("feature1".to_string(), Some("master".to_string())),
             ("feature2".to_string(), Some("master".to_string())),
@@ -50,7 +99,23 @@ mod tests {
         ]
         .into_iter()
         .map(|(x, y)| (x.to_string(), y.map(|s| s.to_string())))
-        .collect();
+        .collect()
+    }
+
+    #[test]
+    fn test_bfs() {
+        // Arrange
+        let edges = get_test_edges();
+        let graph = Graph::from_edges(&edges);
+
+        // Act
+        graph.dfs();
+    }
+
+    #[test]
+    fn test_from_edges() {
+        // Arrange
+        let edges = get_test_edges();
 
         // Act
         let graph = Graph::from_edges(&edges);
